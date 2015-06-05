9c43659da234d3ec399139b4ca02bee1063b2679
nguyenist <stephanie@silicalabs.com>
2015-06-05 13:56:50 -0400

    update

--- js/app.js
+++ js/app.js
@@ -1,93 +1,17 @@
+ // portfolio
+ $('.gallery ul li a').click(function() {
+     var itemID = $(this).attr('href');
+     $('.gallery ul').addClass('item_open');
+     $(itemID).addClass('item_open');
+     return false;
+ });
+ $('.close').click(function() {
+     $('.port, .gallery ul').removeClass('item_open');
+     return false;
+ });
 
-
-// On load we show first img only
-$('.carousel img:first-child').addClass('active');
-
-$('.showNext').on('click', function (){
-    if ($('.active').is(':last-child')) {
-        $('.carousel img:first-child').addClass('active');
-        $('.carousel img:last-child').removeClass('active');
-    } else {
-        // go to next image
-        $('.active').next().addClass('active').prev().removeClass('active');
-    }
-
-});
-
-
-$('.showBack').on('click', function (){
-    if ($('.active').is(':first-child')) {
-        $('.carousel img:last-child').addClass('active');
-        $('.carousel img:first-child').removeClass('active');
-    } else {
-        $('.active').prev().addClass('active').next().removeClass('active');
-    }
-});
-
-
-
-
-/*var timer = setInterval(nextImage, 4000);*/
-
-var curImage = 0;
-var numImages = 5;
-
-
-$('#container').click(function() {
-/*function nextImage() {*/
-    var e;
-    // remove showMe class from current image
-    e = document.getElementById("slideimg" + curImage);
-    removeClass(e, "showMe");
-
-    // compute next image
-    curImage++;
-    if (curImage > numImages - 1) {
-        curImage = 0;
-    }
-
-    // add showMe class to next image
-    e = document.getElementById("slideimg" + curImage);
-    addClass(e, "showMe");
-});
-
-function addClass(elem, name) {
-    var c = elem.className;
-    if (c) c += " ";  // if not blank, add a space separator
-    c += name;
-    elem.className = c;
-}
-
-function removeClass(elem, name) {
-    var c = elem.className;
-    elem.className = c.replace(name, "").replace(/\s+/g, " ").replace(/^\s+|\s+$/g, "");  // remove name and extra blanks
-}
-
-
-
-
-
-
-
-
-
-
-// Parallax
-if('ontouchstart' in window == false){
-  $('body').mousemove(function(e){
-    var moveX = (e.pageX * -1 / 25);
-    var moveY = (e.pageY * -1 / 25);
-    $('#big img').css({'top': moveX + 'px', 'left':  moveY + 'px'});
-  });
-}
-
-
-
-
-
-
-
-
-
-
-
+ $(".gallery ul li a").click(function() {
+     $('html, body').animate({
+         scrollTop: parseInt($("#top").offset().top)
+     }, 400);
+ });
